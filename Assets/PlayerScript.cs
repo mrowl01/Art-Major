c1b55030a97e1eafbaee53e58b32ecb6f0f2322b
Chris <christopherortega56@gmail.com>
2017-11-19 00:25:21 -0600

    Added art, hallmonitor boss, moving , glitchy jumping , and box colliders

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerScript : MonoBehaviour {
+	Rigidbody2D rigidBody;
+	[SerializeField] float jump = 0.10f;
+	[SerializeField] float speed= 0.50f; 
+
+	private bool isJumping = false; 
+	// Use this for initialization
+	void Start () 
+	{
+		rigidBody = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HoriMovement();
+
+	}
+	void FixedUpdate()
+	{
+		Jumping ();
+	}
+
+	void HoriMovement()
+	{
+		Vector3 movement; 
+
+		float x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		movement.x = x;
+		movement.y = 0;
+		movement.z = 0; 
+		transform.Translate (movement* speed); 
+	}
+	void Jumping()
+	{
+		if (CrossPlatformInputManager.GetButton("Jump") && isJumping == false)
+			{
+			isJumping = true; 
+				rigidBody.AddForce(transform.up * jump);
+
+			}
+		isJumping = false;
+	
+	}
+}
+	
